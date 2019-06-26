c991b71be3ac4c8c3d1ff68602cbae2a563e8312
Brandon Nordberg <nordberg5780@gmail.com>
2019-06-26 00:23:28 -0600

    Updated liri.js with proper keys and shes working

--- liri.js
+++ liri.js
@@ -1,262 +1,126 @@
 require("dotenv").config();
 
+// Packages
+var fs = require("fs");
+var keys = require("./keys.js")
+var axios = require("axios");
+var moment = require("moment");
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify)
 
-//set variables requiring npm packages etc
+// User Input
+var userChoice = process.argv[2];
+var userQuery = process.argv[3];
 
-var keys = require('./keys.js');
-var Twitter = require('twitter');
-var spotify = require('spotify');
-var request = require('request');
-var inquirer = require('inquirer');
-var fs = require('fs');
-
-//set variables for inquirer prompt functions
-
-var twitterPrompt = inquirer.createPromptModule();
-var spotifyPrompt = inquirer.createPromptModule();
-var moviePrompt = inquirer.createPromptModule();
-
-//set variable to grab hidden Twitter keys
-
-var client = new Twitter({
-    consumer_key: keys.twitterKeys.consumer_key,
-    consumer_secret: keys.twitterKeys.consumer_secret,
-    access_token_key: keys.twitterKeys.access_token_key,
-    access_token_secret: keys.twitterKeys.access_token_secret
-});
-
-//Use inquirer for selection of commands
-
-inquirer.prompt([{
-    type: "list",
-    message: "What command would you like to run?",
-    choices: ["my-tweets", "spotify-this-song", "movie-this", "do-what-it-says"],
-    name: "commands"
+for (var i = 4; i < process.argv.length; i++) {
+    if (i > 4 && i < process.argv.length) {
+        userQuery += "+" + process.argv[i]; 
+    }
+    else {
+        userQuery += process.argv[i];
+    }
 }
 
-]).then(function (user) {
-    console.log(JSON.stringify(user, null, 2));
+switch (userChoice) {
+    case "concert-this":
+        concertThis();
+        break;
+    case "spotify-this-song":
+        spotifyThis();
+        break;
+    case "movie-this":
+        movies();
+        break;
+    case "do-what-it-says":
+        doThis();
+        break;
+    default:
+        logThis("Please enter a valid search term, such as {concert-this},");
+        logThis("{spotify-this-song}, {movie-this}, or {do-what-it-says}");
+        break;
+}
 
-    //==========================================Twitter===============================================
+// Spotify Function
+function spotifyThis() {
 
-    if (user.commands === "my-tweets") {
-
-        //prompt user to confirm if they reallllly want to read my awesome Tweets
-
-        twitterPrompt([{
-            type: "confirm",
-            message: "Are you sure you want to view my past posts?",
-            name: "confirm",
-            default: true
-        }
-
-        ]).then(function (user) {
-
-            //If user confirms yes and there is no error, my last 20 Tweets will be listed
-
-            if (user.confirm === true) {
-                //console.log("test");
-
-                var params = {
-                    screen_name: "ashmac10",
-                    count: 20
-                };
-
-                //console.log(params.screen_name);
-
-                client.get("statuses/user_timeline", params, function (error, response) {
-                    if (!error) {
-                        for (var i = 0; i < response.length; i++) {
-                            var time = response[i].created_at;
-                            var tweets = response[i].text;
-                            var user = params.screen_name;
-                            console.log("============================================");
-                            console.log("");
-                            console.log("----------------" + time + "--------------------------");
-                            console.log("");
-                            console.log("@" + user + " tweeted:");
-                            console.log("");
-                            console.log(tweets);
-                            console.log("");
-                            console.log("============================================");
-                        }
-                        //console.log(tweets);
-                    } else {
-                        console.log(error);
-                    }
-                });
-
-                //if user decides to not view my Tweets, console log response
-
-            } else {
-                console.log("============================================");
-                console.log("");
-                console.log("Probably a wise choice! Try looking up a movie or song!");
-                console.log("");
-                console.log("============================================");
-            }
-        });
-
-        //=======================================Spotify===============================================
-
-    } else if (user.commands === "spotify-this-song") {
-
-        //prompt user to type in song to lookup - 'The Sign' by Ace of Base set as default
-        spotifyPrompt([{
-            type: "input",
-            message: "What song should I look up?",
-            name: "song",
-            default: "The Sign Ace of Base"
-        }
-
-            //set up Spotify search for Track based on "song" input by user
-
-        ]).then(function (response) {
-
-            spotify.search({
-                type: 'track',
-                query: response.song,
-                limit: 1
-            }, function (err, data) {
-                if (err) {
-                    console.log('Error occurred: ' + err);
-                    return;
-
-                    //set up base path for finding information through Spotify API
-
-                } else {
-                    var trackName = data.tracks.items;
-
-                    //for loop through results to pick out desired information
-
-                    for (var i = 0; i < 1; i++) {
-
-                        var trackData = trackName[i];
-
-                        var artists = trackData.artists[0].name;
-                        var song = trackData.name;
-                        var preview = trackData.preview_url;
-                        var album = trackData.album.name;
-
-                        //console log out the information found on the track provided by user
-
-                        console.log("============================================");
-                        console.log("");
-                        console.log("Artist: " + artists);
-                        console.log("~~~~~~~~~~~~~~~~~~~~~");
-                        console.log("Song Title: " + song);
-                        console.log("~~~~~~~~~~~~~~~~~~~~~");
-                        console.log("Album: " + album);
-                        console.log("~~~~~~~~~~~~~~~~~~~~~");
-                        console.log("Song Preview: " + preview);
-                        console.log("");
-                        console.log("============================================");
-                    }
-                }
-            });
-        });
-
-
-        //===========================================OMDB================================================
-
-    } else if (user.commands === "movie-this") {
-
-        moviePrompt([{
-            type: "input",
-            message: "What movie should I look up?",
-            name: "movie",
-            default: "Mr. Nobody"
-        }
-
-        ]).then(function (response) {
-
-            //run a request to the OMDB API with the movie specified by user
-
-            request("http://www.omdbapi.com/?t=" + response.movie + "&y=&plot=short&r=json", function (error, response, body) {
-
-                // If there is no error, and the request is successful (i.e. if the response status code is 200)
-
-                if (!error && response.statusCode === 200) {
-
-                    // Parse the body of the site and recover the info needed
-
-                    console.log("======================================");
-                    console.log("");
-                    console.log("Title: " + JSON.parse(body).Title);
-                    console.log("~~~~~~~~~~~~~~~~~~~~~");
-                    console.log("Release Date: " + JSON.parse(body).Year);
-                    console.log("~~~~~~~~~~~~~~~~~~~~~");
-                    console.log("IMBD rating is: " + JSON.parse(body).imdbRating);
-                    console.log("~~~~~~~~~~~~~~~~~~~~~");
-                    console.log("Produced in (country): " + JSON.parse(body).Country);
-                    console.log("~~~~~~~~~~~~~~~~~~~~~");
-                    console.log("Main language: " + JSON.parse(body).Language);
-                    console.log("~~~~~~~~~~~~~~~~~~~~~");
-                    console.log("Plot: " + JSON.parse(body).Plot);
-                    console.log("~~~~~~~~~~~~~~~~~~~~~");
-                    console.log("Actor's include: " + JSON.parse(body).Actors);
-                    console.log("");
-                    console.log("======================================");
-                }
-            });
-        });
-
-        //===============================================FS============================================
-
-    } else if (user.commands === "do-what-it-says") {
-        // This block of code will read from the "random.txt" file.
-        // It's important to include the "utf8" parameter or the code will provide stream data (garbage)
-        // The code will store the contents of the reading inside the variable "data"
-        fs.readFile("random.txt", "utf8", function (error, data) {
-            if (error) {
-                console.log(error);
-            } else {
-                //  var dataArr = data.split(",");
-
-                //  console.log(dataArr);
-
-                spotify.search({
-                    type: 'track',
-                    query: data,
-                    limit: 1
-                }, function (err, data) {
-                    if (err) {
-                        console.log('Error occurred: ' + err);
-                        return;
-
-                        //set up base path for finding information through Spotify API
-
-                    } else {
-                        var trackName = data.tracks.items;
-
-                        //for loop through results to pick out desired information
-
-                        for (var i = 0; i < 1; i++) {
-
-                            var trackData = trackName[i];
-
-                            var artists = trackData.artists[0].name;
-                            var song = trackData.name;
-                            var preview = trackData.preview_url;
-                            var album = trackData.album.name;
-
-                            //console log out the information found on the track provided by user
-
-                            console.log("============================================");
-                            console.log("");
-                            console.log("Artist: " + artists);
-                            console.log("~~~~~~~~~~~~~~~~~~~~~");
-                            console.log("Song Title: " + song);
-                            console.log("~~~~~~~~~~~~~~~~~~~~~");
-                            console.log("Album: " + album);
-                            console.log("~~~~~~~~~~~~~~~~~~~~~");
-                            console.log("Song Preview: " + preview);
-                            console.log("");
-                            console.log("============================================");
-                        }
-                    }
-                });
-            }
-        });
+    // Catch empty input
+    if (!userQuery) {
+        userQuery = "The Sign Ace of Base";
     }
-});
+
+    spotify.search({type: "track", query: userQuery}, function(err, data) {
+        if (err) {
+            logThis(err);
+        }
+
+        var userSong = data.tracks.items;
+        logThis("Artist: " + userSong[0].artists[0].name);
+        logThis("Song Name: " + userSong[0].name);
+        logThis("Preview Link: " + userSong[0].preview_url);
+        logThis("Album: " + userSong[0].album.name);
+    });
+};
+
+function movies() {
+
+    if (!userQuery) {
+        userQuery = "Mr. Nobody";
+        logThis("If you haven't watched 'Mr. Nobody,' then you should: <http://www.imdb.com/title/tt0485947/>");
+        logThis("It's on Netflix!");
+    }
+    
+    axios.get("http://www.omdbapi.com/?t=" + userQuery + "&y=&plot=short&apikey=5870c63c")
+    .then(function(response) {
+
+        logThis("Title: " + response.data.Title);
+        logThis("Year Released: " + response.data.Year);
+        logThis("IMDB rating: " + response.data.imdbRating);
+        logThis("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+        logThis("Country/Countries Produced: " + response.data.Country);
+        logThis("Language: " + response.data.Language);
+        logThis("Plot: " + response.data.Plot);
+        logThis("Cast: " + response.data.Actors);
+    });
+};
+
+function concertThis() {
+
+    if (!userQuery) {
+        userQuery = "Slayer";
+    }
+    axios.get("https://rest.bandsintown.com/artists/" + userQuery + "/events?app_id=codingbootcamp")
+    .then(function(response) {
+        for (var i = 0; i < response.data.length; i++) {
+
+            logThis("Venue Name: "+ response.data[i].venue.name);
+            logThis("Venue Location: " + response.data[i].venue.city + ", " + response.data[i].venue.country);
+            logThis("Date of the Event: " + moment(response.data[i].datetime).format("L"));
+        }
+    });
+}
+
+function doThis () {
+    fs.readFile("random.txt", "utf8", function(err, data) {
+
+        if (err) {
+            logThis(err);
+        }
+
+        var readArray = data.split(",");
+
+        userQuery = readArray[1];
+
+        spotifyThis(userQuery);
+    })
+};
+
+function logThis (logQuery) {
+
+    console.log(logQuery);
+
+    fs.appendFile("log.txt", logQuery, function(err) {
+        if (err) {
+            return logThis("Error: " + err);
+        }
+    });
+};
